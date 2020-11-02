f2b70196aedb61d1845952a54e8ddca560cc3f8e
T0shik <antonwieslander@hotmail.com>
2020-11-02 17:52:21 +0000

    published product migrations

---
+++ src/RawCoding.Shop.Database/Migrations/20201102174907_published.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RawCoding.Shop.Database.Migrations
+{
+    public partial class published : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Published",
+                table: "Products",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Published",
+                table: "Products");
+        }
+    }
+}
